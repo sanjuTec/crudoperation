2f0af43af03c835979c34ff0fd9b6a0cd8eb848a
kelum <kelumnagodavithana1997@gmail.com>
2024-06-10 12:27:45 +0530

    updtaed

--- src/main/java/com/crudoparation/crudoperation/controller/EmployeeController.java
+++ src/main/java/com/crudoparation/crudoperation/controller/EmployeeController.java
@@ -2,18 +2,17 @@ package com.crudoparation.crudoperation.controller;
 
 import com.crudoparation.crudoperation.dto.EmployeeDTO;
 import com.crudoparation.crudoperation.dto.ResponseDTO;
+import com.crudoparation.crudoperation.entity.Employee;
 import com.crudoparation.crudoperation.service.EmployeeService;
 import com.crudoparation.crudoperation.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.PrivateKey;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/employee")
@@ -40,7 +39,7 @@ public class EmployeeController {
             return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
         }else{
             responseDTO.setCode(VarList.RSP_FAIL);
-            responseDTO.setMessage("Error");
+            responseDTO.setMessage("Error in Save Employee");
             responseDTO.setContent(null);
             return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
         }
@@ -50,5 +49,53 @@ public class EmployeeController {
             responseDTO.setContent(null);
             return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    }
+
+    //update Employee
+    @PutMapping(value = "/updateEmployee")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO){
+        try {
+            String response = employeeService.updateEmployee(employeeDTO);
+
+            if(response.equals("00")){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }else if(response.equals("01")){
+                responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("Not a Registered Employee");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }else{
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("Error in update Employee");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        }catch(Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // fetch all employee data
+    @GetMapping("/getAllEmployees")
+    public ResponseEntity getAllEmployee(){
+        try {
+            List<EmployeeDTO> employeeDTOList = employeeService.getAllEmployee();
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(employeeDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+        }catch(Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/crudoparation/crudoperation/service/EmployeeService.java
+++ src/main/java/com/crudoparation/crudoperation/service/EmployeeService.java
@@ -6,8 +6,12 @@ import com.crudoparation.crudoperation.repo.EmployeeRepo;
 import com.crudoparation.crudoperation.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -27,4 +31,22 @@ public class EmployeeService {
         }
 
     }
+
+    public String updateEmployee(EmployeeDTO employeeDTO){
+        if(employeeRepo.existsById(employeeDTO.getEmployeeId())){
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+        }else{
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+    public List<EmployeeDTO> getAllEmployee(){
+        List<Employee> employeeList = employeeRepo.findAll();
+        return  modelMapper.map(employeeList, new TypeToken<ArrayList<EmployeeDTO>>(){
+
+        }.getType());
+    }
+
+
 }
